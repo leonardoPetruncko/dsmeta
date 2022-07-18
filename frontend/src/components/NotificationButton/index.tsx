ea425a306da5aa563c889a028af87fc427d6f5a1
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-07-17 21:43:55 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,16 +1,25 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css'
 
-function NotificationButton() {
-    return(
-    
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar"/>
-        </div>
+type Props = {
+    saleId: number;
+}
+function handleClick(id :number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then( Response => {
+        console.log("dale")
+    })
+}
 
+function NotificationButton({saleId} : Props) {
+    return (
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>
     )
-   }
-   
-   export default NotificationButton
-   
+}
+
+export default NotificationButton
